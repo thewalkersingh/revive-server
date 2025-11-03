d4f5ad7aa430873902783ae883df575c8652c3e6
the.walker <the.walkersingh@hotmail.com>
2025-11-03 22:20:02 +0530

    first commit

---
+++ src/main/java/com/horizon/reviveserver/ReviveServerApplication.java
@@ -0,0 +1,13 @@
+package com.horizon.reviveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviveServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horizon/reviveserver/ReviveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizon.reviveserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
